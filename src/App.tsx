3476730dc77ad1b76bae58d1f3a2b0e5f6cc047c
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-06-09 11:57:38 +0900

    fixed merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
-import CreateItinerary from "./components/CreateItinerary/CreateItinerary"
+import Navbar from "./components/Navbar/Navbar";
+import Landing from "./components/Landing/Landing";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import LanguageToggle from "./components/LanguageToggle";
+
+// Authentication use
+import { useAuth } from "./contexts/AuthContext"
+import { AuthProvider } from "./contexts/AuthContext"
+import SignUp from "./components/Authentication/SignUp/SignUp"
+import LogIn from "./components/Authentication/LogIn/LogIn"
+import ForgotPassword from "./components/Authentication/ForgotPassword/ForgotPassword"
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+
 
 const App = () => {
 
