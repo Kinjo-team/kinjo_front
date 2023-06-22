dfc3a99333fb34c779595ec855472ea63e064dda
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-06-22 19:02:23 +0900

    feat: beginning to modify SearchItineraries component to allow for autopopulating/predictive search terms

--- src/components/SearchItineraries/SearchItineraries.tsx
+++ src/components/SearchItineraries/SearchItineraries.tsx
@@ -1,20 +1,49 @@
 import { useState, FormEvent, ChangeEvent} from 'react';
 import './SearchItineraries.scss';
 import DisplayItineraries from '../DisplayItineraries/DisplayItineraries';
+import debounce from 'lodash/debounce';
 
 const SearchItineraries = () => {
   const [searchOption, setSearchOption] = useState('Name');
   const [searchValue, setSearchValue] = useState('');
   const [showResults, setShowResults] = useState(false);
   const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [autocompleteResults, setAutocompleteResults] = useState<any[]>([]);
 
   const handleSearchOption = (option: string) => {
     setSearchOption(option);
   };
 
+  const autocomplete = async (searchValue: string) => {
+    const response = await fetch(`http://localhost:8000/autocomplete?option=${searchOption}&value=${searchValue}`, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+      if (response.ok) {
+        const searchData = await response.json();
+        console.log("This is the data from autocomplete:", searchData);
+        setAutocompleteResults(searchData);
+      } else {
+        console.error('Autocomplete request failed');
+      }
+  };
+
+  const debouncedAutocomplete = debounce(autocomplete, 300);
+
   const handleSearchValue = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
+    if (event.target.value.length > 0) {
+      debouncedAutocomplete(event.target.value);
+    } else {
+      setAutocompleteResults([]);
+    }
   };
+
+  const handleAutocompleteClick = (itinerary: any) => {
+    setSearchValue(itinerary.itinerary_name);
+    setAutocompleteResults([]);
+  }
 
   // encodeURIComponent
   const handleSubmit = async (event : FormEvent<HTMLFormElement>) => {
@@ -48,7 +77,10 @@ const SearchItineraries = () => {
         <>
           <form className="search-itineraries" onSubmit={handleSubmit}>
               <div className='searchbar'>
-                <select name='search-option' id='search-option' defaultValue="Itinerary" onChange={(event) => handleSearchOption(event.target.value)}>
+                <select name='search-option'
+                        id='search-option' 
+                        defaultValue="Itinerary" 
+                        onChange={(event) => handleSearchOption(event.target.value)}>
                   <option value='Name'>Kinjo</option>
                   <option value='Tag'>Tag</option>
                   <option value='User'>User</option>
@@ -59,14 +91,31 @@ const SearchItineraries = () => {
                     onChange={handleSearchValue}
                     className='search-input'
                 />
+                {autocompleteResults.length > 0 && (
+                  <div className="autocomplete-results">
+                    {autocompleteResults.map((result, index) => (
+                      <div key={index}
+                           className="autocomplete-result" 
+                           onClick={() => handleAutocompleteClick(result)}>
+                    {result.itinerary_name}  {/* or itinerary_name or other field as per your requirement */}
+                  </div>
+                ))}
+              </div>
+            )}
                 <span className="material-symbols-outlined search-icon">
                   search
                 </span>
               </div>
-            {showResults && <DisplayItineraries itineraries={searchResults} toggleShowResults={toggleShowResults} />}
+            {showResults && 
+              <DisplayItineraries itineraries={searchResults} 
+              toggleShowResults={toggleShowResults} />}
           </form>
         </>
       );
 };
     
 export default SearchItineraries;
+
+/*
+
+*/
