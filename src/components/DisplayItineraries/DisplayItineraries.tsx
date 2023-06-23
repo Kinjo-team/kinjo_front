c4081f2997f1a16edbaa0fb700b6d7a6adcdf71f
RatedYK <terrence0101@yahoo.com>
2023-06-23 13:55:25 +0900

    feat: add scrolling to search results

--- src/components/DisplayItineraries/DisplayItineraries.tsx
+++ src/components/DisplayItineraries/DisplayItineraries.tsx
@@ -15,27 +15,30 @@ const DisplayItineraries: React.FC<DisplayItinerariesProps> = ({ itineraries, to
             <div className='display-itineraries--container'>
                 <button onClick={toggleShowResults} className='display-itineraries-close-btn'>X</button>
                 <h1><span>{itineraries.length}</span> search results matching <span className='search-value'>"{searchValue}"</span></h1>
-                {itineraries.map((itinerary) => (
-                    <Link to={`/itinerary/${itinerary.itinerary_id}`} key={itinerary.itinerary_id}>
-                        <section className="display-itineraries--card"
-                            key={itinerary.itinerary_id}>
-                            <div className="part1">
-                                <h3>{itinerary.itinerary_name}</h3>
-                                <img src={itinerary.itinerary_image_url} alt="" />
-                            </div>
-                            <div className='part2'>
-                                <p>{itinerary.itinerary_descr}</p>
-                                <span className='tags-container'>Tags: {itinerary.itinerary_tags.map((tag : string) => {
-                                    return (<div className='tag'>{tag}</div>)
-                                })}</span>
-                            </div>
-                            <div className='part3'>
-                                <img src={itinerary.user.user_img} alt="" />
-                                <h4>{itinerary.user.username}</h4>
-                            </div>
-                        </section>
-                    </Link>
-                ))}
+                <div className='display-itineraries-cards-container'>
+                    {itineraries.map((itinerary) => (
+                        <Link to={`/itinerary/${itinerary.itinerary_id}`} key={itinerary.itinerary_id}>
+                            <section className="display-itineraries--card"
+                                key={itinerary.itinerary_id}>
+                                <div className="part1">
+                                    <h3>{itinerary.itinerary_name}</h3>
+                                    <img src={itinerary.itinerary_image_url} alt="" />
+                                </div>
+                                <div className='part2'>
+                                    <p>{itinerary.itinerary_descr}</p>
+                                    <span className='tags-container'>Tags: {itinerary.itinerary_tags.map((tag : string) => {
+                                        return (<div className='tag'>{tag}</div>)
+                                    })}</span>
+                                </div>
+                                <div className='part3'>
+                                    <img src={itinerary.user.user_img} alt="" />
+                                    <h4>{itinerary.user.username}</h4>
+                                </div>
+                            </section>
+                        </Link>
+                    ))}
+
+                </div>
                 <button onClick={toggleShowResults} className='close-btn'>Close</button>
             </div>
         </main> 
