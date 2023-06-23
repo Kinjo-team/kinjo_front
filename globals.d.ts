efdd2c0f83830ce1d032c2e825d43699ab52a37f
Alex Fisher <a.fisher615@gmail.com>
2023-06-23 14:34:49 +0900

    added prototype (front-back compatible?) Itineraries interface)

--- globals.d.ts
+++ globals.d.ts
@@ -34,10 +34,25 @@ export interface Location {
     image_urls: string[];
 }
 
-export interface userData {
+export interface UserData {
     user_id: number;
     username: string;
     user_email: string;
+}
+
+//Prototype Itinerary definition aiming for front-back cross-compatibility 
+export interface Itinerary {
+    itinerary_id: number;
+    firebase_uuid: string;
+    itinerary_name: string           
+    itinerary_descr_en: string
+    itinerary_tags: string[];
+    kinjo_coords: number[]
+    user: {};
+    likes: {}
+    bookmarks: {}
+    comments: {}
+    itinerary_image_url: String?
 }
 
 // Interface Definitions from the backend;
