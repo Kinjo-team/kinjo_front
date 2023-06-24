326b09d0f5030b006c8e2013ba7719f1189997e3
RatedYK <terrence0101@yahoo.com>
2023-06-24 14:24:13 +0900

    feat: add delete button and confirmation on profile

--- src/components/DashboardCompos/DKinjoCard/DKinjoCard.tsx
+++ src/components/DashboardCompos/DKinjoCard/DKinjoCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import DeleteConfirmation from './DeleteConfirmation'
+
 import './DKinjoCard.scss'
 
 type DKinjoCardProps = {
@@ -6,13 +8,31 @@ type DKinjoCardProps = {
 }
 
 const DKinjoCard = ({kinjo} : DKinjoCardProps) => {
+  const [showConfirmation, setShowConfirmation] = useState<boolean>(false)
+
+  // FUNCTIONS
+  const handleDeleteKinjo = async () => {
+    await fetch(`${process.env.REACT_APP_BACKEND_URL}itineraries/${kinjo.itinerary_id}`, {
+      method: 'DELETE'
+    })
+    window.location.reload()
+  }
+
+  function toggleConfirmation(event: any) {
+    event.stopPropagation()
+    setShowConfirmation(!showConfirmation)
+  }
   return (
-    <div className='dkinjo-card--container'>
-        <h1 className='dkinjo-card-title'>{kinjo.itinerary_name}</h1>
+    <>
+      {showConfirmation && <DeleteConfirmation kinjoName={kinjo.itinerary_name} handleDeleteKinjo={handleDeleteKinjo} toggleConfirmation={toggleConfirmation} />}
+      <div key={kinjo.itinerary_id} className='dkinjo-card--container'>
+        <button onClick={toggleConfirmation}>X</button>
+        <a className='dkinjo-card-title' href={`/kinjo/${kinjo.itinerary_id}`}><h1>{kinjo.itinerary_name}</h1></a>
         <div className='dkinjo-card-img'>
             <img src={kinjo.itinerary_image_url} alt={kinjo.itinerary_name} />
         </div>
-    </div>
+      </div>
+    </>
   )
 }
 
