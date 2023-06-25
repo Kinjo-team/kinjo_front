65ead6b98d29a91223c208a25de40b3755b7248e
garrettomi <omigarrett@gmail.com>
2023-06-25 19:09:48 +0900

    Fix: Update after code review

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -222,7 +222,7 @@ const Map: React.FC<MapProps> = ({
     const inputElement = searchInputRef.current;
     if (inputElement && inputElement.value) {
       const query = inputElement.value;
-      const apiKey = "0be542e0feab4cc9a51ccfc191f4dcc3";
+      const apiKey = process.env.REACT_APP_LEAFLET_API_KEY;
       try {
         const response = await fetch(
           `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
@@ -270,7 +270,7 @@ const Map: React.FC<MapProps> = ({
       <MapContainer
         center={defaultPosition}
         zoom={13}
-        style={{ height: "500px", width: "100%" }}
+        style={{ height: "600px", width: "100%" }}
         className="create-map"
         maxBounds={japanBounds}
         minZoom={5}
@@ -332,6 +332,7 @@ const Map: React.FC<MapProps> = ({
                   name="loc_name"
                   id="loc_name"
                   placeholder="Tanaka's Coffee"
+                  maxLength={30}
                   value={newLocationData.loc_name}
                   onChange={handleInputChange}
                   required
